84da1342873f7a87f26f290398e8ce43bf812194
Nina <gutovskanina@gmail.com>
2024-04-27 09:58:08 +0200

    add cross origin

--- spring-protoPFeedback/src/main/java/protopnet/mlprototypesfeedbackcollector/controllers/HomeController.java
+++ spring-protoPFeedback/src/main/java/protopnet/mlprototypesfeedbackcollector/controllers/HomeController.java
@@ -2,6 +2,7 @@ package protopnet.mlprototypesfeedbackcollector.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -12,7 +13,7 @@ public class HomeController {
     public String showHomePage(Model model) {
         return "HomePage";
     }
-
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/home")
     public String showHomePageForRegisteredUser(Model model) {
         return "HomePageForRegisteredUser";
